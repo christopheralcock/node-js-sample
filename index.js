7096798e6f78c939483558360bd8068f496c9250
Christopher Alcock <christopher.alcock@gmail.com>
2017-01-27 13:38:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Here is my website that is node on azure')
 })
 
 app.listen(app.get('port'), function() {
